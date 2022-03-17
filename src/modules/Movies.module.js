f054ee30fcb04e2047ae92345c13daa0d489b775
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-17 19:47:00 +0330

    Create Movies.module.js

---
+++ src/modules/Movies.module.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const cheerio = require("cheerio");
+const URL = require('../config').Settings.Movies
+
+module.exports = async ({ name }) => {
+
+    let Movies = [];
+
+    return new Promise((resolve) => {
+        axios.get(URL + name).then(response => {
+
+            const $ = cheerio.load(response.data);
+            const table = $("#thaTableIndeed").children("tr");
+
+            table.each(async (index, item) => {
+
+                if (index > 0) {
+
+                    const link = $(item).children("td:nth-child(2)").children("a").attr("href");
+                    const title = $(item).children("td:nth-child(2)").children("a").attr("title");
+                    const category = $(item).children("td:nth-child(1)").children("a").text();
+                    const size = $(item).children("td:nth-child(3)").text();
+
+                    if (link.substring(0, 5) === "https") {
+                        Movies.push({ category, title, size, link });
+                    }
+
+                }
+            });
+
+            resolve(Movies)
+        }).catch(e => { console.log(e?.response?.data?.message || e) });
+    });
+};
