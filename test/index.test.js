8a7519c221749ad5bad65850d0224847451f737a
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-17 20:07:28 +0330

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -6,9 +6,29 @@ const {
     Tv
 } = require('medias-info')
 
-PCGame({ name: 'gta' }).then(res => { console.log(res) })
-ConsoleGame({ name: 'gta' }).then(res => { console.log(res) })
-Songs({ name: 'gta' }).then(res => { console.log(res) })
-Moives({ name: 'gta' }).then(res => { console.log(res) })
-Tv({ name: 'gta' }).then(res => { console.log(res) })
+PCGame({ name: '' }).then(res => { console.log(res) })
+ConsoleGame({ name: '' }).then(res => { console.log(res) })
+Songs({ name: '' }).then(res => { console.log(res) })
+Moives({ name: '' }).then(res => { console.log(res) })
+Tv({ name: '' }).then(res => { console.log(res) })
 
+// --------- OR --------- //
+
+const Data /* Or Every Var You Want */ = require('medias-info')
+
+Data.PCGame({ name: '' }).then(res => { console.log(res) })
+Data.ConsoleGame({ name: '' }).then(res => { console.log(res) })
+Data.Songs({ name: '' }).then(res => { console.log(res) })
+Data.Moives({ name: '' }).then(res => { console.log(res) })
+Data.Tv({ name: '' }).then(res => { console.log(res) })
+
+/************************************
+    
+    * Available function are : 👇
+    * PCGame (Required Name Input)
+    * ConsoleGame (Required Name Input)
+    * Songs (Required Name Input)
+    * Moives (Required Name Input)
+    * Tv (Required Name Input)
+    
+************************************/
