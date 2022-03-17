a8b5b1647d75cf5dd02539f194e2c055ce1d7427
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-17 19:39:20 +0330

    Added `Movies` URL

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 exports.PCGame = require('./modules/PCGame.module')
 exports.ConsoleGame = require('./modules/ConsoleGame.module')
 exports.Songs = require('./modules/Songs.module')
-exports.Moives = require('./modules/Moives.module')
+exports.Moives = require('./modules/Movies.module')
 exports.Tv = require('./modules/Tv.module')
