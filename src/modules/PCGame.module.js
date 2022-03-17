a8ee17793b5201aeaf2d7e3d3edb11681200ef5a
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-17 19:43:40 +0330

    Update PCGame.module.js

--- src/modules/PCGame.module.js
+++ src/modules/PCGame.module.js
@@ -2,3 +2,17 @@ const axios = require('axios');
 const cheerio = require("cheerio");
 const URL = require('../config').Settings.PCGame
 
+module.exports = async ({ name }) => {
+
+    let PC = [];
+
+    return new Promise((resolve) => {
+        axios.get(URL + name).then(response => {
+
+            const $ = cheerio.load(response.data);
+            const table = $("#thaTableIndeed").children("tr");
+
+            resolve(PC)
+        }).catch(e => { console.log(e?.response?.data?.message || e) });
+    });
+};
