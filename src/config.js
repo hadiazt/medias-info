4d8ce426e171732d42cb9704dbfb50cdbe923b87
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-17 19:39:33 +0330

    Added `TV` URL

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ exports.Settings = {
     PCGame: "https://knaben.eu/search/?cat=PC&q=",
     ConsoleGame: "https://knaben.eu/search/?cat=Console&q=",
     Audio: "https://knaben.eu/search/?cat=Audio&q=",
-
+    Movies: "https://knaben.eu/search/?cat=Movies&q=",
+    TV: "https://knaben.eu/search/?cat=TV&q="
 }
