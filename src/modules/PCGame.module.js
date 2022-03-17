48dc59e5c95aea9c1ca3880f082e5d93af27fc5d
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-17 19:44:06 +0330

    Update PCGame.module.js

--- src/modules/PCGame.module.js
+++ src/modules/PCGame.module.js
@@ -12,6 +12,19 @@ module.exports = async ({ name }) => {
             const $ = cheerio.load(response.data);
             const table = $("#thaTableIndeed").children("tr");
 
+            table.each(async (index, item) => {
+
+                if (index > 0) {
+
+                    const link = $(item).children("td:nth-child(2)").children("a").attr("href");
+                    const title = $(item).children("td:nth-child(2)").children("a").attr("title");
+                    const category = $(item).children("td:nth-child(1)").children("a").text();
+                    const size = $(item).children("td:nth-child(3)").text();
+
+
+                }
+            });
+
             resolve(PC)
         }).catch(e => { console.log(e?.response?.data?.message || e) });
     });
